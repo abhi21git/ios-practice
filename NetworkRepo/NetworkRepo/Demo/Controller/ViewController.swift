b08089c86cd1aafe7a67a76cf61fd27920f3b1f0
Simran Rout <43809488+simranrout@users.noreply.github.com>
2023-02-23 21:45:33 +0530

    [Set Up] Network Layer Project Created

---
+++ NetworkRepo/NetworkRepo/Demo/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NetworkRepo
+//
+//  Created by Simran Rout on 19/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
---
+++ NetworkRepo/NetworkRepo/Network Layer/APICall.swift
@@ -0,0 +1,9 @@
+//
+//  APICall.swift
+//  NetworkRepo
+//
+//  Created by Simran Rout on 19/02/23.
+//
+
+import Foundation
+class 
