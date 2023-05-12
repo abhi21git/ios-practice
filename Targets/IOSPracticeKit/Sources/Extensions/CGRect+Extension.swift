efac12aaf9a86318bd8e9f8d718d60e55539ea03
Abhishek Maurya <42537316+abhi21git@users.noreply.github.com>
2023-05-12 18:51:40 +0530

    [Migrate] Migrated to tuist

---
+++ Targets/IOSPracticeKit/Sources/Extensions/CGRect+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  CGRect+Extension.swift
+//  IOSPracticeUI
+//
+//  Created by Abhishek Maurya on 13/05/23.
+//  Copyright © 2023 practice.com All rights reserved.
+//
+
+import UIKit
+
+extension CGRect {
+    public var halfHeight: CGFloat {
+        return height / 2
+    }
+    
+    public var halfWidth: CGFloat {
+        return width / 2
+    }
+}
